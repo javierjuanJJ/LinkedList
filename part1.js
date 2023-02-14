d792e50b2c3d7506b72e661208791d0b1edf8f0b
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-14 12:11:10 +0100

    Initial commit

--- part1.js
+++ part1.js
@@ -28,4 +28,14 @@ const printLinkedList = (head) => {
   }
 }
 
+const linkedListValues = (head) => {
+  const values = [];
+  let current = head;
+  while (current != null){
+      values.push(current.val);
+      current = current.next;
+  }
+  return values;
+}
+
 printLinkedList(a);
