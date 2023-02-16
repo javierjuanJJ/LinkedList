4b3fd6f4a6e866842c711797e2b82e89c612ceaf
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-16 12:06:32 +0100

    Initial commit

--- part1.js
+++ part1.js
@@ -9,21 +9,40 @@ class Node {
 const a = new Node("a");
 const b = new Node("b");
 const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
-
 a.next = b;
 b.next = c;
-c.next = d;
-d.next = e;
-e.next = f;
+// a -> b -> c
 
-const reverseList = (head, prev = null) => {
-    if (head === null) return prev;
-    const next = head.next;
-    head.next = prev;
-    return reverseList(next, head);
+const x = new Node("x");
+const y = new Node("y");
+const z = new Node("z");
+x.next = y;
+y.next = z;
+// x -> y -> z
+
+const zipperLists = (head1, head2) => {
+    const head = head1;
+    let tail = head;
+    let current1 = head1.next;
+    let current2 = head2;
+    let count = 0;
+
+    while (current1 !== null && current2 !== null) {
+        if (count % 2 === 0) {
+            tail.next = current2;
+            current2 = current2.next;
+        } else {
+            tail.next = current1;
+            current1 = current1.next;
+        }
+        tail = tail.next;
+        count += 1;
+    }
+
+    if (current1 !== null) tail.next = current1;
+    if (current2 !== null) tail.next = current2;
+
+    return head;
 };
 
-console.log(reverseList(a));
+console.log(zipperLists(a, x));
