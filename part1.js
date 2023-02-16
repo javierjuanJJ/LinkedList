590ca8c5f13e01bca95f6d5486d96aec81bd2a5c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-02-16 11:41:10 +0100

    Initial commit

--- part1.js
+++ part1.js
@@ -19,16 +19,11 @@ c.next = d;
 d.next = e;
 e.next = f;
 
-const reverseList = (head) => {
-    let current = head;
-    let prev = null;
-    while (current !== null) {
-        const next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-    }
-    return prev;
+const reverseList = (head, prev = null) => {
+    if (head === null) return prev;
+    const next = head.next;
+    head.next = prev;
+    return reverseList(next, head);
 };
 
 console.log(reverseList(a));
